c955b3c62cf8305357a9af3268aff3f631224e44
bielbcs <bielcotrimsv@gmail.com>
2023-03-10 20:26:24 -0300

    testes de integração rotas: POST /cart/:id, PUT /cart/:id/:quantity e DELETE /cart/:id

--- src/tests/integration/cart.test.js
+++ src/tests/integration/cart.test.js
@@ -6,7 +6,8 @@ const jwt = require('jsonwebtoken');
 const INITIAL_PRODUCTS = require('../../../seeder');
 const Product = require('../../models/products.model');
 const Cart = require('../../models/cart.model');
-const { MOCKED_CART } = require('../mocks/cartMocks');
+const { MOCKED_CART, MOCKED_RETURNED_PRODUCTS, 
+  MOCKED_CART_WITH_A_PRODUCT } = require('../mocks/cartMocks');
 const { RETURNED_USER_MOCK } = require('../mocks/userMocks');
 
 const { expect } = chai;
@@ -17,15 +18,73 @@ afterEach(() => {
   sinon.restore();
 })
 
-describe('GET /cart', function() {
-  it('Deve retornar todos os produtos', async function() {
+describe('CART', function() {
+  beforeEach(() => {
     sinon.stub(jwt, 'verify').returns({_doc: RETURNED_USER_MOCK});
-    sinon.stub(Cart, 'findOne').resolves(MOCKED_CART);
-    const result = await chai
-      .request(app)
-      .get('/cart')
-
-    expect(result.status).to.be.equals(200);
-    expect(result.body).to.be.deep.equals(MOCKED_CART);
   });
-});
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('GET /cart', function() {
+    it('Deve retornar todos os produtos', async function() {
+      sinon.stub(Cart, 'findOne').resolves(MOCKED_CART);
+      const result = await chai
+        .request(app)
+        .get('/cart')
+  
+      expect(result.status).to.be.equals(200);
+      expect(result.body).to.be.deep.equals(MOCKED_CART);
+    });
+  });
+  
+  describe('POST /cart/:id', function() {
+    it('Deve ser possível incluir um produto no carrinho', async function() {  
+  
+      sinon.stub(Product, 'findOne').returns({
+        select: sinon.stub().resolves(MOCKED_RETURNED_PRODUCTS[0])
+      });
+  
+      sinon.stub(Cart, 'findOne')
+        .onFirstCall()
+        .resolves({ ...MOCKED_CART, save: sinon.stub().resolves() })
+        .onSecondCall()
+        .resolves(MOCKED_CART_WITH_A_PRODUCT);
+  
+      const result = await chai
+        .request(app)
+        .post(`/cart/${MOCKED_RETURNED_PRODUCTS[0]._id}`)
+  
+      expect(result.body).to.be.deep.equals(MOCKED_CART_WITH_A_PRODUCT);
+      expect(result.status).to.be.equals(201);
+    })
+  });
+  
+  describe('PUT /cart/:id/:quantity', function() {
+    it('Deve atualizar a quantidade de um produto espeficido', async function() {
+      sinon.stub(Cart, 'updateOne').resolves();
+      sinon.stub(Cart, 'findOne').resolves(MOCKED_CART_WITH_A_PRODUCT);
+      const result = await chai
+        .request(app)
+        .put(`/cart/${MOCKED_RETURNED_PRODUCTS}/10`)
+  
+      expect(result.status).to.be.equals(201);
+      expect(result.body).to.be.deep.equals(MOCKED_CART_WITH_A_PRODUCT);
+    });
+  });
+
+  describe('DELETE /cart/:id', function() {
+    it('Deve remover um produto especifico do carrinho', async function() {
+      sinon.stub(Cart, 'findOneAndUpdate').resolves(MOCKED_CART_WITH_A_PRODUCT);
+
+      const result = await chai
+        .request(app)
+        .delete(`/cart/${MOCKED_RETURNED_PRODUCTS}`)
+  
+      expect(result.status).to.be.equals(200);
+      expect(result.body).to.be.deep.equals(MOCKED_CART_WITH_A_PRODUCT);
+    });
+  });
+})
+
--- src/tests/mocks/cartMocks.js
+++ src/tests/mocks/cartMocks.js
@@ -5,4 +5,50 @@ const MOCKED_CART = {
   __v: 0
 }
 
-module.exports = { MOCKED_CART }
+const MOCKED_CART_WITH_A_PRODUCT = {
+  _id: "640ba7adb9e80500397adfbd",
+  user_id: "640ba7adb9e80500397adfbb",
+  products: [{
+    _id: "640b8e147bdb3a158371f2a7",
+    name: "Hamburguer",
+    price: 12,
+    image_url: "http://localhost:3001/images/hamburguer.jpg",
+    quantity: 1
+  },],
+  __v: 0
+}
+
+const MOCKED_RETURNED_PRODUCTS = [
+  {
+    "_id": "640b8e147bdb3a158371f2a7",
+    "name": "Hamburguer",
+    "price": 12,
+    "image_url": "http://localhost:3001/images/hamburguer.jpg"
+  },
+  {
+    "_id": "640b8e147bdb3a158371f2a8",
+    "name": "Pizza de Calabreza",
+    "price": 35,
+    "image_url": "http://localhost:3001/images/pizza.jpg"
+  },
+  {
+    "_id": "640b8e147bdb3a158371f2a9",
+    "name": "Batata frita",
+    "price": 7.5,
+    "image_url": "http://localhost:3001/images/batata_frita.jpg"
+  },
+  {
+    "_id": "640b8e147bdb3a158371f2aa",
+    "name": "Coca-cola 2L",
+    "price": 10.5,
+    "image_url": "http://localhost:3001/images/coca_cola.jpg"
+  },
+  {
+    "_id": "640b8e147bdb3a158371f2ab",
+    "name": "Coxinha",
+    "price": 5.25,
+    "image_url": "http://localhost:3001/images/coxinha.jpg"
+  }
+]
+
+module.exports = { MOCKED_CART, MOCKED_RETURNED_PRODUCTS, MOCKED_CART_WITH_A_PRODUCT }
